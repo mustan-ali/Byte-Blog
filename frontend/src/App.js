e81481c3f844368201bc9c168fbae3d59d204228
Mustan Ali <74346675+mustan-ali@users.noreply.github.com>
2024-04-09 12:16:09 +0500

    add navbar & footer components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
 
 function App() {
   return (
     <>
+      <Navbar />
 
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+      </Routes>
+
+      <Footer />
     </>
   );
 }

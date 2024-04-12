8d233a573532686a3065481d5896c3f2592e9241
Mustan Ali <74346675+mustan-ali@users.noreply.github.com>
2024-04-12 07:34:03 +0000

    set up database connection

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 require('dotenv').config()
 const PORT = process.env.PORT;
 
+const ConnectDatabase = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log("Connected to database");
+    }
+    catch (error) {
+        console.log("Error connecting to database: ", error);
+    }
+}
+
 app.listen(PORT, () => {
+    ConnectDatabase();
     console.log(`Server is running on port ${PORT}`);
 });

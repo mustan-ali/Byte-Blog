c38be1fd94557f01609ca9893274665e60f5ef27
Mustan Ali <74346675+mustan-ali@users.noreply.github.com>
2024-04-14 11:57:47 +0500

    add comment controllers

---
+++ backend/controllers/commentController.js
@@ -0,0 +1,52 @@
+const User = require('../models/User');
+const Comment = require('../models/Comment');
+const Post = require('../models/Post');
+require('dotenv').config()
+
+
+const newComment = async (req, res) => {
+    try {
+        const newComment = new Comment(req.body)
+        const savedComment = await newComment.save()
+        res.status(200).json(savedComment)
+    }
+    catch (err) {
+        res.status(500).json(err)
+    }
+}
+
+
+const getPostComment = async (req, res) => {
+    try {
+        const comments = await Comment.findById(req.params.id);
+        res.status(200).json(comments);
+    }
+    catch (error) {
+        res.status(500).json(error);
+    }
+}
+
+
+const updateComment = async (req, res) => {
+    try {
+        const updatedComment = await Comment.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        res.status(200).json(updatedComment);
+    }
+    catch (error) {
+        res.status(500).json(error);
+    }
+}
+
+
+const deleteComment = async (req, res) => {
+    try {
+        await Comment.findByIdAndDelete(req.params.id);
+        res.status(200).json("Comment has been deleted");
+    }
+    catch (error) {
+        res.status(500).json(error);
+    }
+}
+
+
+module.exports = { newComment, getPostComment, updateComment, deleteComment };

6a51c094fad8c56896c4d08eb4fdc442fd0cbde7
Mustan Ali <74346675+mustan-ali@users.noreply.github.com>
2024-04-15 15:44:44 +0500

    update CORS config

--- backend/index.js
+++ backend/index.js
@@ -5,12 +5,13 @@ const cookieParser = require('cookie-parser');
 require('dotenv').config()
 const cors = require('cors');
 const PORT = process.env.PORT;
+const FrontendURL = process.env.FRONTEND_URL;
 const authRoute = require('./routes/auth');
 const userRoute = require('./routes/users');
 const postRoute = require('./routes/posts');
 const commentRoute = require('./routes/comments');
 
-app.use(cors());
+app.use(cors({ origin: { FrontendURL }, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoute)

7287f2fa536e336315a365ee4d956cc81876bc94
Mustan Ali <74346675+mustan-ali@users.noreply.github.com>
2024-04-15 16:51:15 +0500

    implement logout feature

--- frontend/src/components/Menu.js
+++ frontend/src/components/Menu.js
@@ -1,5 +1,25 @@
+import { useContext } from "react";
+import { UserContext } from "../context/UserContext";
+
 export const Menu = () => {
-    const user = true;
+    const { user, setUser } = useContext(UserContext);
+
+    const handleLogout = async () => {
+        try {
+            await fetch("http://localhost:5000/api/auth/logout", {
+                method: "POST",
+                credentials: "include",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
+            setUser(null);
+        }
+        catch (err) {
+            console.error(err);
+        }
+    }
+
     return (
         <div className="bg-black w-[200px] z-10 flex flex-col items-start absolute top-12 right-6 md:right-32 rounded-md p-4 space-y-4">
             {!user && <h3 className="text-white text-sm hover:text-gray-500 cursor-pointer">Login</h3>}
@@ -7,7 +27,7 @@ export const Menu = () => {
             {user && <h3 className="text-white text-sm hover:text-gray-500 cursor-pointer">Profile</h3>}
             {user && <h3 className="text-white text-sm hover:text-gray-500 cursor-pointer">Write</h3>}
             {user && <h3 className="text-white text-sm hover:text-gray-500 cursor-pointer">My Blogs</h3>}
-            {user && <h3 className="text-white text-sm hover:text-gray-500 cursor-pointer">Logout</h3>}
+            {user && <h3 onClick={handleLogout} className="text-white text-sm hover:text-gray-500 cursor-pointer">Logout</h3>}
         </div>
     )
 }

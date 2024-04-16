5c9adf71ff487d13a1cb85ffcaf31ae8034066d7
Mustan Ali <74346675+mustan-ali@users.noreply.github.com>
2024-04-16 13:46:06 +0500

    implement home posts feature

--- frontend/src/components/HomePosts.js
+++ frontend/src/components/HomePosts.js
@@ -1,25 +1,23 @@
-export const HomePosts = () => {
+export const HomePosts = ({ post }) => {
     return (
         <div className="w-full flex my-8 space-x-4">
             {/* left */}
             <div className="w-[350px] h-[200px] flex justify-center items-center">
-                <img src="https://images.unsplash.com/photo-1569396116180-7fe09fa16dd8?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" className="h-full w-full object-cover" />
+                <img src={post.photo} alt="" className="h-full w-full object-cover" />
             </div>
             {/* right */}
             <div className="flex flex-col w-[65%]">
                 <h1 className="text-xl font-bold md:mb-2 mb-1 md:text-2xl">
-                    Test Title
+                    {post.title}
                 </h1>
                 <div className="flex mb-2 text-sm font-semibold text-gray-500 items-center justify-between md:mb-4">
-                    <p>@Username</p>
+                    <p>@{post.username}</p>
                     <div className="flex space-x-2 text-sm">
-                        <p>01/01/2024</p>
-                        <p>12:00</p>
+                        <p>{new Date(post.updatedAt).toString()}</p>
                     </div>
                 </div>
-                <p className="text-sm ">lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptates.</p>
+                <p className="text-sm ">{post.description.slice(0, 200) + "... Read More"}</p>
             </div>
-
         </div>
     )
 }
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,11 +1,37 @@
+import { useState, useEffect } from "react";
 import HomePosts from "../components/HomePosts";
+const URL = process.env.REACT_APP_BACKEND_URL;
 
 export const Home = () => {
+
+    const [posts, setPosts] = useState([]);
+
+    const fetchPosts = async () => {
+        try {
+            const res = await fetch(`${URL}/api/posts`, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
+            const data = await res.json();
+            setPosts(data);
+            console.log(data);
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        fetchPosts();
+    }, []);
+
     return (
         <div className="px-8 md:px-[200px] min-h-[80vh]">
-            <HomePosts />
-            <HomePosts />
-            <HomePosts />
+            {posts.map((post) => (
+                <HomePosts key={post._id} post={post} />
+            ))}
         </div>
     )
 }

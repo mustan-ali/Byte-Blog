6714121fca68f6c056eaed0f4b91fa75202fa97a
Mustan Ali <74346675+mustan-ali@users.noreply.github.com>
2024-04-16 15:10:02 +0500

    add link to individual posts in Home page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import HomePosts from "../components/HomePosts";
 const URL = process.env.REACT_APP_BACKEND_URL;
-
 export const Home = () => {
 
     const [posts, setPosts] = useState([]);
@@ -30,7 +30,9 @@ export const Home = () => {
     return (
         <div className="px-8 md:px-[200px] min-h-[80vh]">
             {posts.map((post) => (
-                <HomePosts key={post._id} post={post} />
+                <Link to={`/post/${post._id}`} key={post._id}>
+                    <HomePosts key={post._id} post={post} />
+                </Link>
             ))}
         </div>
     )

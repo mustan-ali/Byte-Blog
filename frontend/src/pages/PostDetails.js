ad99bb59fa40ddef056f4fe198cd322cafa1a8da
Mustan Ali <74346675+mustan-ali@users.noreply.github.com>
2024-04-16 16:28:41 +0500

    update image source url

--- frontend/src/pages/PostDetails.js
+++ frontend/src/pages/PostDetails.js
@@ -71,7 +71,7 @@ export const PostDetails = () => {
                         <p>{new Date(post.updatedAt).toString().slice(16, 21)}</p>
                     </div>
                 </div>
-                <img src={post.photo} className="w-full  mx-auto mt-8" alt="" />
+                <img src={`${URL}/images/${post.photo}`} className="w-full  mx-auto mt-8" alt="" />
                 <p className="mx-auto mt-8">{post.description}</p>
                 <div className="flex items-center mt-8 space-x-4 font-semibold">
                     <p>Categories:</p>

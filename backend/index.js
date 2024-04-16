25be3fe056e65e11666d4089a8f8fd8f8e0c4148
Mustan Ali <74346675+mustan-ali@users.noreply.github.com>
2024-04-16 16:27:12 +0500

    add image static file serving

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ const cookieParser = require('cookie-parser');
 const multer = require('multer');
 require('dotenv').config()
 const cors = require('cors');
+const path = require('path');
 const PORT = process.env.PORT;
 const FrontendURL = process.env.FRONTEND_URL;
 const authRoute = require('./routes/auth');
@@ -15,6 +16,7 @@ const commentRoute = require('./routes/comments');
 app.use(cors({ origin: { FrontendURL }, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
+app.use("/images", express.static(path.join(__dirname, "/images")));
 app.use("/api/auth", authRoute)
 app.use("/api/users", userRoute)
 app.use("/api/posts", postRoute)

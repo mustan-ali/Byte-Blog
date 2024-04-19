d8939eebd67e9edb7aa420bc06b583f78fd5002d
Mustan Ali <74346675+mustan-ali@users.noreply.github.com>
2024-04-19 11:29:02 +0500

    add delete functionality

--- frontend/src/components/Comment.js
+++ frontend/src/components/Comment.js
@@ -1,28 +1,47 @@
 import { useContext } from 'react';
-import { BiEdit } from 'react-icons/bi';
 import { MdDelete } from 'react-icons/md'
 import { UserContext } from '../context/UserContext'
+import { Navigate } from 'react-router-dom';
 
-export const Comment = ({ comment }) => {
+export const Comment = ({ c }) => {
 
     const { user } = useContext(UserContext);
+
+    const handleDeleteComment = async () => {
+        try {
+            const res = await fetch(`${URL}/api/comments/${c._id}`, {
+                method: "DELETE",
+                credentials: "include",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
+
+            if (res.ok) {
+                Navigate("/post/" + c.postId);
+            }
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
 
     return (
         <div className="px-2 py-2 bg-gray-200 rounded-lg my-2">
             <div className="flex items-center justify-between">
-                <h3 className="font-bold text-gray-600">@{comment.author}</h3>
+                <h3 className="font-bold text-gray-600">@{c.author}</h3>
                 <div className="flex justify-center items-center space-x-4">
-                    <p>{new Date(comment.updatedAt).toString().slice(0, 15)}</p>
-                    <p>{new Date(comment.updatedAt).toString().slice(16, 24)}</p>
-                    {user?._id === comment.userId && (
+                    <p>{new Date(c.updatedAt).toString().slice(0, 15)}</p>
+                    <p>{new Date(c.updatedAt).toString().slice(16, 24)}</p>
+                    {user?.id === c?.userId ?
                         <div className="flex items-center justify-center space-x-2">
-                            <p className="cursor-pointer"><BiEdit /></p>
-                            <p className="cursor-pointer"><MdDelete /></p>
+                            <p onClick={handleDeleteComment} className="cursor-pointer"><MdDelete /></p>
                         </div>
-                    )}
+                        : ""}
                 </div>
             </div>
-            <p className="px-4 mt-2">lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptates.</p>
+            <p className="px-4 mt-2">{c.comment}</p>
+
         </div>
     )
 }

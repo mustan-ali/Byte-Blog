90a19f83ff78c08d487d7b081eb3fbd0cade603a
Mustan Ali <74346675+mustan-ali@users.noreply.github.com>
2024-04-19 10:33:16 +0500

    add functionality to edit a post

--- frontend/src/pages/EditPost.js
+++ frontend/src/pages/EditPost.js
@@ -1,10 +1,81 @@
-import { useState } from 'react'
+import { useState, useContext, useEffect } from 'react'
+import { UserContext } from '../context/UserContext';
+import { useParams, useNavigate } from 'react-router-dom'
 import { ImCross } from 'react-icons/im'
+const URL = process.env.REACT_APP_BACKEND_URL;
 
-export const EditPost = () => {
-
+const EditPost = () => {
+    const postId = useParams().id;
+    const { user } = useContext(UserContext);
+    const navigate = useNavigate();
+    const [title, setTitle] = useState('')
+    const [description, setDescription] = useState('')
+    const [file, setFile] = useState(null)
     const [category, setCategory] = useState('')
     const [categories, setCategories] = useState([])
+
+    const fetchPost = async () => {
+        try {
+            const response = await fetch(`${URL}/api/posts/${postId}`);
+            const data = await response.json();
+            setTitle(data.title);
+            setDescription(data.description);
+            setFile(data.photo);
+            setCategories(data.categories);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    const handleUpdate = async (e) => {
+        e.preventDefault();
+        const post = {
+            title,
+            description,
+            username: user.username,
+            userId: user._id,
+            categories
+        }
+
+        if (file) {
+            const data = new FormData();
+            const filename = Date.now() + file.name;
+            data.append("img", filename);
+            data.append("file", file);
+            post.photo = filename;
+
+            try {
+                await fetch(`${URL}/api/upload`, {
+                    method: 'POST',
+                    body: data,
+                    credentials: 'include'
+                });
+            } catch (err) {
+                console.log(err);
+            }
+        }
+
+        try {
+            const res = await fetch(`${URL}/api/posts/${postId}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                },
+                body: JSON.stringify(post),
+                credentials: 'include'
+            });
+            const data = await res.json();
+            navigate(`/post/${data._id}`);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    useEffect(() => {
+        fetchPost();
+        // eslint-disable-next-line
+    }, [postId]);
 
     const addCategory = () => {
         setCategories([...categories, category])
@@ -20,9 +91,9 @@ export const EditPost = () => {
     return (
         <div className='px-6 md:px-[200px] mt-8'>
             <h1 className='font-bold md:text-2xl text-xl '>Update a post</h1>
-            <form className='w-full flex flex-col space-y-4 md:space-y-8 mt-4 mb-4'>
-                <input type="text" placeholder='Enter post title' className='px-4 py-2 outline-none' />
-                <input type="file" className='px-4' />
+            <form className='w-full flex flex-col space-y-4 md:space-y-8 mt-4'>
+                <input onChange={(e) => setTitle(e.target.value)} value={title} type="text" placeholder='Enter post title' className='px-4 py-2 outline-none' />
+                <input onChange={(e) => setFile(e.target.files[0])} type="file" className='px-4' />
                 <div className='flex flex-col'>
                     <div className='flex items-center space-x-4 md:space-x-8'>
                         <input value={category} onChange={(e) => setCategory(e.target.value)} className='px-4 py-2 outline-none' placeholder='Enter post category' type="text" />
@@ -38,8 +109,8 @@ export const EditPost = () => {
                         ))}
                     </div>
                 </div>
-                <textarea rows={12} cols={30} className='px-4 py-2 outline-none' placeholder='Enter post description' />
-                <button className='bg-black w-full md:w-[20%] mx-auto text-white font-semibold px-4 py-2 md:text-xl text-lg'>Update</button>
+                <textarea onChange={(e) => setDescription(e.target.value)} value={description} rows={12} cols={30} className='px-4 py-2 outline-none' placeholder='Enter post description' />
+                <button onClick={handleUpdate} className='bg-black w-full md:w-[20%] mx-auto text-white font-semibold px-4 py-2 md:text-xl text-lg'>Update</button>
             </form>
         </div>
     )

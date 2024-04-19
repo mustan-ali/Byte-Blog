e7820edbe9a08185513e1cf116e4ade37f442071
Mustan Ali <74346675+mustan-ali@users.noreply.github.com>
2024-04-19 13:09:33 +0500

    add links to menu items

--- frontend/src/components/Menu.js
+++ frontend/src/components/Menu.js
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import { UserContext } from "../context/UserContext";
+import { Link } from "react-router-dom";
 
 export const Menu = () => {
     const { user, setUser } = useContext(UserContext);
@@ -22,11 +23,11 @@ export const Menu = () => {
 
     return (
         <div className="bg-black w-[200px] z-10 flex flex-col items-start absolute top-12 right-6 md:right-32 rounded-md p-4 space-y-4">
-            {!user && <h3 className="text-white text-sm hover:text-gray-500 cursor-pointer">Login</h3>}
-            {!user && <h3 className="text-white text-sm hover:text-gray-500 cursor-pointer">Register</h3>}
-            {user && <h3 className="text-white text-sm hover:text-gray-500 cursor-pointer">Profile</h3>}
-            {user && <h3 className="text-white text-sm hover:text-gray-500 cursor-pointer">Write</h3>}
-            {user && <h3 className="text-white text-sm hover:text-gray-500 cursor-pointer">My Blogs</h3>}
+            {!user && <h3 className="text-white text-sm hover:text-gray-500 cursor-pointer"><Link to="/login">Login</Link></h3>}
+            {!user && <h3 className="text-white text-sm hover:text-gray-500 cursor-pointer"><Link to="/register">Register</Link></h3>}
+            {user && <h3 className="text-white text-sm hover:text-gray-500 cursor-pointer"><Link to={"/profile/" + user.id}>Profile</Link></h3>}
+            {user && <h3 className="text-white text-sm hover:text-gray-500 cursor-pointer"><Link to="/write">Write</Link></h3>}
+            {user && <h3 className="text-white text-sm hover:text-gray-500 cursor-pointer"><Link to={"/myblogs/" + user.id}>My Blogs</Link></h3>}
             {user && <h3 onClick={handleLogout} className="text-white text-sm hover:text-gray-500 cursor-pointer">Logout</h3>}
         </div>
     )

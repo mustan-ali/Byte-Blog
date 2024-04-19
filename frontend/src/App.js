585c124cea4f2eb7b6052afb9e332e9cee840be2
Mustan Ali <74346675+mustan-ali@users.noreply.github.com>
2024-04-19 13:09:15 +0500

    add MyBlogs page to routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import PostDetails from './pages/PostDetails';
 import CreatePost from './pages/CreatePost';
 import EditPost from './pages/EditPost';
 import Profile from './pages/Profile';
+import MyBlogs from './pages/MyBlogs';
 import { UserContextProvider } from './context/UserContext';
 
 function App() {
@@ -18,8 +19,9 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/login" element={<Login />} />
-        <Route exact path="/profile/:id" element={<Profile />} />
         <Route exact path="/register" element={<Register />} />
+        <Route exact path="/profile/:id" element={<Profile />} />
+        <Route exact path="/myblogs/:id" element={<MyBlogs />} />
         <Route exact path="/write" element={<CreatePost />} />
         <Route exact path="/edit/:id" element={<EditPost />} />
         <Route exact path="/post/:id" element={<PostDetails />} />

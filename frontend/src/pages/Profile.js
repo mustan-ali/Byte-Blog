69c32d67067032ed51a50a197fdb97310fe4990b
Mustan Ali <74346675+mustan-ali@users.noreply.github.com>
2024-04-26 10:16:36 +0500

    update Profile page to display user posts

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -1,7 +1,7 @@
 import { useEffect, useState, useContext } from "react";
 import { UserContext } from "../context/UserContext";
-import ProfilePost from "../components/ProfilePost";
-import { Navigate } from "react-router-dom";
+import { Navigate, Link } from "react-router-dom";
+import HomePosts from "../components/HomePosts";
 const URL = process.env.REACT_APP_BACKEND_URL;
 
 export const Profile = () => {
@@ -10,6 +10,7 @@ export const Profile = () => {
     const [username, setUsername] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [posts, setPosts] = useState([]);
 
     const fetchUserDetails = async () => {
         try {
@@ -71,8 +72,27 @@ export const Profile = () => {
         }
     }
 
+    const fetchUserPosts = async () => {
+        try {
+            if (user?.id) {
+                const res = await fetch(`${URL}/api/posts/user/` + user.id, {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                });
+                const data = await res.json();
+                setPosts(data);
+            }
+        }
+        catch (err) {
+            console.error(err);
+        }
+    }
+
     useEffect(() => {
         fetchUserDetails();
+        fetchUserPosts();
         // eslint-disable-next-line
     }, [user?.id]);
 
@@ -80,9 +100,11 @@ export const Profile = () => {
         <div className="min-h-[80vh] px-8 md:px-[200px] mt-8 flex md:flex-row flex-col-reverse md:items-start items-start">
             <div className="flex flex-col md:w-[70%] w-full mt-8 md:mt-0">
                 <h1 className="text-xl font-bold mb-4">Your posts:</h1>
-                <ProfilePost />
-                <ProfilePost />
-                <ProfilePost />
+                {posts?.map((p) => (
+                    <Link to={`/post/${p._id}`} key={p._id}>
+                        <HomePosts key={p._id} post={p} />
+                    </Link>
+                ))}
             </div>
             <div className="md:sticky md:top-12  flex justify-start md:justify-end items-start md:w-[30%] w-full md:items-end ">
                 <div className=" flex flex-col space-y-4 items-start">
